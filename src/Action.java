e71ddfbf5d650ba25717f99d35e21da129820780
ibrownlee <ibrownlee@ycp.edu>
2024-12-05 16:16:11 -0500

    Update Action.java

--- src/Action.java
+++ src/Action.java
@@ -1,3 +1,22 @@
+/******************************************************************
+*                        Party Members' Actions                   *
+*                                                                 *
+* PROGRAMMER:Chloe Barber and Bella Brownlee                      *
+* COURSE: CS201                                                   *
+* DATE: 12/05/2024                                                *
+* REQUIREMENT: Final Project                                      *
+*                                                                 *
+* DESCRIPTION:                                                    *
+* The following program creates the class Action and adds fields  *
+* for action1 and action2. It also includes get methods for both  *
+* attacks.                                                        *
+*                                                                 *
+* COPYRIGHT: This code is copyright (C) 2024 Chloe Barber, Bella  *
+* Brownlee, and Dean Zeller.                                      *
+*                                                                 *
+* CREDITS: This code was written in its entirety by Chloe Barber  *
+* and Bella Brownlee.                                             *
+******************************************************************/
 public class Action {
     private Attack action1;
     private Attack action2;
